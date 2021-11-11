881315367839edeedf872fbc207c54c51771c447
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-11-11 19:21:06 +0530

    SQL Queries started

--- my.sql
+++ my.sql
@@ -1,1 +1,21 @@
-CREATE DATABASE student;
+-- create a table
+
+CREATE TABLE students (
+ 
+  id INTEGER PRIMARY KEY,
+
+  name TEXT NOT NULL,
+
+  gender TEXT NOT NULL
+
+);
+
+-- insert some values
+
+INSERT INTO students(id,name,gender) VALUES (1, 'Ryan', 'M'),(3,'raman','F');
+
+INSERT INTO students VALUES (2, 'Joanna', 'F');
+
+-- fetch some values
+
+SELECT * FROM students ;
